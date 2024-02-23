4065573627dfa7a94a1af0bf77a1cf6104430bc9
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-02-23 20:17:51 +0000

    feat: add typeorm setup, user entity, validations and CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userservice: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.userservice.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userservice.findOne(id);
+  }
+
+  @Get()
+  findAll(@Query('email') email: string) {
+    return this.userservice.find(email);
+  }
+
+  @Delete()
+  deleteUser(@Param('id') id: string) {
+    return this.userservice.remove(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userservice.update(id, body);
+  }
+}
