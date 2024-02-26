a0405ce23bf19de4ddcde6d5d0eba56140e8e692
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-02-26 11:59:31 +0000

    feat: use interceptors, refactor exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,26 +3,34 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userservice: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.userservice.create(body.email, body.password);
+    return this.userservice.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userservice.findOne(id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.userservice.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   @Get()
