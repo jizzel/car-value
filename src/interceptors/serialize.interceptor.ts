64f7dfe16e9c93d71f5051b27a6b0c467e2c4245
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-02-26 12:35:26 +0000

    ref: refactor serializer and dto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,16 +1,28 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { plainToInstance } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
+
+interface ClassConstructor {
+  new (...args: any[]);
+}
+export const Serialize = (dto: ClassConstructor) => {
+  return UseInterceptors(new SerializeInterceptor(dto));
+};
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       map((data: any) => {
-        return plainToInstance(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,25 +8,25 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-  constructor(private userservice: UsersService) {}
+  constructor(private userService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return this.userservice.create(body.email, body.password);
+    return this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    const user = await this.userservice.findOne(id);
+    const user = await this.userService.findOne(id);
     if (!user) {
       throw new NotFoundException('User not found');
     }
@@ -35,16 +35,16 @@ export class UsersController {
 
   @Get()
   findAll(@Query('email') email: string) {
-    return this.userservice.find(email);
+    return this.userService.find(email);
   }
 
   @Delete()
   deleteUser(@Param('id') id: string) {
-    return this.userservice.remove(id);
+    return this.userService.remove(id);
   }
 
   @Patch('/:id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.userservice.update(id, body);
+    return this.userService.update(id, body);
   }
 }
